aded96e09a0a3d3aae134287f482065202251772
yuvalc2006 <yuvalcohen107@gmail.com>
2025-02-13 07:09:27 +0200

    68

---
+++ ex69.py
@@ -0,0 +1,22 @@
+# my original solution was not optimal, made this with a bit of outside help
+
+from tqdm import tqdm
+
+if __name__ == '__main__':
+    n = 1_000_000
+    phi = [1] * (n + 1)
+    phi[1] = 1
+    prime = [True] * (n + 1)
+
+    for i in tqdm(range(2, n + 1)):
+        if not prime[i]:
+            continue
+
+        num = i
+
+        while num <= n:
+            prime[num] = False
+            phi[num] /= (1 - 1/i)
+            num += i
+    max_div = max(phi)
+    print(phi.index(max_div), max_div)
