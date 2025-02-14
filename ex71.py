7fa1926474834f712d3575719e7ea0715611385f
yuvalc2006 <yuvalcohen107@gmail.com>
2025-02-14 03:18:15 +0200

    71

---
+++ ex71.py
@@ -0,0 +1,45 @@
+import math
+
+from tqdm import tqdm
+
+
+def intercepts(a, b):
+    for i in a:
+        if i in b:
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    n = 1_000_000
+    prime_factors = [set() for i in range(n + 2)]
+    target = 3 / 7
+    closest = 1 / 8
+    closest_num = 1
+
+    for i in tqdm(range(2, n + 1)):
+        curr_primes = prime_factors[i]
+        if len(curr_primes) == 0:
+            curr_primes.add(i)
+            num = i
+            while num <= n:
+                prime_factors[num].add(i)
+                num += i
+
+        if i == 3:
+            continue
+        if i == 6:
+            pass
+        num = math.ceil(i / target)
+        curr_div = i / num
+        while num <= n and curr_div > closest:
+            if not intercepts(curr_primes, prime_factors[num]):
+                if target - curr_div < 0:
+                    pass
+                closest_num = i
+                closest = curr_div
+                break
+            num += 1
+            curr_div = i / num
+
+    print(closest_num, closest)
